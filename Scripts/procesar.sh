a1c1c308aaae30f6bf217f62245a0b0ab091255b
Valentin <valentindimenna@gmail.com>
2023-07-04 19:01:38 -0300

    Procesar.sh procesa todas la imagenees de la carpeta
    Signed-off-by: Valentin <valentindimenna@gmail.com>

--- Scripts/procesar.sh
+++ Scripts/procesar.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+ 
+carpeta="../Datasets/imgs"
 
-nombre_archivo=$1
-echo "$nombre_archivo"
+if [ -z "$(ls  "$carpeta")" ];then 
+	echo "La carpeta esta vacia no hay imagenes por procesar"
+	exit 1
+fi
+for imagen in "$carpeta"/*; do
+	if echo "$imagen" | grep ^\.\./Datasets/imgs/[A-Z][a-z]*\.jpeg$
+		then
+		echo "nombre de archivo Valido"
+		convert $imagen -gravity center -resize 512x512+0+0 $imagen
 
-if echo "$nombre_archivo" | grep ^[A-Z][a-z]*\.jpeg$
-then
-	echo "nombre de archivo Valido"
-	convert Datasets/imgs/$nombre_archivo -gravity center -resize 512x512+0+0 Datasets/imgs/$nombre_archivo
+	else
+		echo "nombre de archivo no valido: "
+		nombre_imagen=$(basename $imagen)
+		echo "$nombre_imagen"
+	fi 
+done
 
-else
-	echo "nombre de archivo no valido"
-fi 
+
